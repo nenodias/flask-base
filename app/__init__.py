1b780adc26fa5a14e8f1f3b708f2c1a0cbb62bf1
Aluno <horacio.dias92@gmail.com>
2016-11-09 21:15:27 -0200

    Adicionando o flask-assets e o jsmin ao projeto

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_assets import Environment, Bundle, ManageAssets
+
 
 app = Flask(__name__)
 
@@ -11,8 +13,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+assets = Environment(app)
+
+
+js = Bundle('js/jquery.js', 'js/bootstrap.js', 'js/bootbox.js','js/search.js',
+             filters='jsmin', output='gen/packed.js')
+assets.register('js_all', js)
+
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+#assets
+manager.add_command("assets", ManageAssets(assets))
 
 from app.controllers import *
